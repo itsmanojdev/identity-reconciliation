71a7789e4d2df618bd106ee7c303a77cb2b25d07
itsmanojdev <manojscmr333@gmail.com>
2025-08-23 14:52:50 +0530

    Refactor db config

--- backend/src/db/connect.ts
+++
@@ -1,7 +0,0 @@
-import { Pool } from "pg"
- 
-const pool = new Pool({ connectionString: process.env.DATABASE_URL})
- 
-export const query = (text: string, params: string[]) => {
-  return pool.query(text, params)
-}
